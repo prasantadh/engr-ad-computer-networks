6f456a9004c14e4f32293b6634ad0935f5563d15
prasantadh <prasantadhikari1111@gmail.com>
2016-06-03 21:24:20 +0400

    preliminary pinger!

--- 2_UDPPinger/UDPPingerServer.py
+++ 2_UDPPinger/UDPPingerServer.py
@@ -10,14 +10,15 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', 12000))
 
 while True:
-    # Generate random number in the range of 0 to 10
-rand = random.randint(0, 10)    
-# Receive the client packet along with the address it is coming from 
-message, address = serverSocket.recvfrom(1024)
-# Capitalize the message from the client
-    message = message.upper()
-    # If rand is less is than 4, we consider the packet lost and do not respond
-    if rand < 4:
-        continue
-    # Otherwise, the server responds    
-    serverSocket.sendto(message, address)
+	print("Ready to Serve")
+	# Generate random number in the range of 0 to 10
+	rand = random.randint(0, 10)    
+	# Receive the client packet along with the address it is coming from 
+	message, address = serverSocket.recvfrom(1024)
+	# Capitalize the message from the client
+	message = message.decode('UTF-8').upper()
+	# If rand is less is than 4, we consider the packet lost and do not respond
+	if rand < 4:
+		continue
+	# Otherwise, the server responds    
+	serverSocket.sendto(bytes(message,'UTF-8'), address)
